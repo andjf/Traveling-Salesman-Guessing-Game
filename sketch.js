6c0beca47be8d476e98af64a518b3c62fc49237e
AndJ <andjferrin@gmail.com>
2020-08-12 13:40:11 -0400

    Added comments and slight changes
    Added comments to sketch.js and made the point value assignment more comprehensive and readable

--- sketch.js
+++ sketch.js
@@ -1,46 +1,97 @@
+// define constants to be used throughout the program 
 const NUMBER_OF_POINTS = 8;
 const NUMBER_OF_PERMUTATIONS = factorial(NUMBER_OF_POINTS);
 
+// variables that are initialized after 
+// the canvas is created for visuals
 var CIRCLE_STROKE_WEIGHT;
 var TOP_BAR_HEIGHT;
 
+// the font to be used for the text
 var myFont;
 
+// preload function loads the font
 function preload() {myFont = loadFont("aller.ttf");}
 
+// points array stores the vector objects of 
+// all of the positions of the points
 var points = Array(NUMBER_OF_POINTS);
+// pointsRadii stores the radii of the points 
+// with the same index in the points array
 var pointsRadii = Array(NUMBER_OF_POINTS);
+// array that is the order of indices going through the 
+// points list that represents the shortest path
+// Ex: [5, 2, 4, 6, 1, 3, 0, 7]
 var optimalIndexOrder = Array(NUMBER_OF_POINTS);
 
-var playerIndexOrder = Array(NUMBER_OF_POINTS);
+// array that stores the indices, in order, of 
+// the points that the player has selected 
+var playerIndexOrder = Array(NUMBER_OF_POINTS).fill(null);
 var currentIndexInPlayerOrder = 0;
 
+// boolean that keeps track of whether the
+// optimal path should be drawn
 var shouldDrawBestPath = false;
+// eventual array that stores the most
+// optimal order of the indices
 var bestPath = -1;
 
 function setup() {
+    textFont(myFont);
     createCanvas(windowWidth, windowHeight);
     const MINIMUM_RADIUS = 25 / 700 * height;
     const MAXIMUM_RADIUS = 50 / 700 * height;
-    
+
     TOP_BAR_HEIGHT = 75 / 700 * height
     CIRCLE_STROKE_WEIGHT = 3 / 700 * height
+
     
+    // loop fills the "points" and "pointsRadii" arrays
     for(var i = 0; i < NUMBER_OF_POINTS; i++) {
-        playerIndexOrder[i] = null;
-        proposedRadius = random(MINIMUM_RADIUS, MAXIMUM_RADIUS);
-        proposedPoint = createVector(random(proposedRadius + CIRCLE_STROKE_WEIGHT, width - proposedRadius - CIRCLE_STROKE_WEIGHT), random(proposedRadius + CIRCLE_STROKE_WEIGHT + TOP_BAR_HEIGHT, height - proposedRadius - CIRCLE_STROKE_WEIGHT));
+        // generates a radius between some minimum and maximum 
+        // that is the proposed radius for the point that is 
+        // currently being inspected (this does not change)
+        var proposedRadius = random(MINIMUM_RADIUS, MAXIMUM_RADIUS);
+
+        // vector object at the top left valid point for a 
+        // point to be created with a certain proposedRadius
+        const topLeft = createVector(
+            proposedRadius + CIRCLE_STROKE_WEIGHT, 
+            proposedRadius + CIRCLE_STROKE_WEIGHT + TOP_BAR_HEIGHT
+        ); 
+
+        // vector object at the bottom right valid point for a 
+        // point to be created with a certain proposedRadius
+        const bottomRight = createVector(
+            width - proposedRadius - CIRCLE_STROKE_WEIGHT, 
+            height - proposedRadius - CIRCLE_STROKE_WEIGHT
+        )
+        
+        var proposedPoint = randomPointInBounds(topLeft, bottomRight);
+        // attempts to find a position for the point on the screen that is not 
+        // intersecting with another existing circle, but if it cannot find a 
+        // suitable position within 50 tries, it just gives it a random position 
+        // on the screen. This ensures that if the screen is too small to fit all 
+        // of the circles, the program won't get stuck in an infinite loop
         var whileCounter = 0;
         while(touchingOtherCircle(proposedPoint, proposedRadius, points, pointsRadii) && whileCounter < 50) {
-            proposedPoint = createVector(random(proposedRadius + CIRCLE_STROKE_WEIGHT, width - proposedRadius - CIRCLE_STROKE_WEIGHT), random(proposedRadius + CIRCLE_STROKE_WEIGHT + TOP_BAR_HEIGHT, height - proposedRadius - CIRCLE_STROKE_WEIGHT));
+            proposedPoint = randomPointInBounds(topLeft, bottomRight);
             whileCounter++;
         }
         points[i] = proposedPoint;
         pointsRadii[i] = proposedRadius;
     }
-    textFont(myFont);
 }
 
+// returns a vector of a point within the bounds specified
+// by the top left and bottom right points of a rectangle
+function randomPointInBounds(p1, p2) {
+    return createVector(random(p1.x, p2.x), random(p1.y, p2.y));
+}
+
+// function return true if a point with a given position 
+// and radius would intersect with any other point inside 
+// of a given array of points with given radii
 function touchingOtherCircle(position, radius, arrayOfOtherPoints, arrayOfRadii) {
     for(var i = 0; i < arrayOfOtherPoints.length; i++) {
         if(arrayOfOtherPoints[i] != null && dist(position.x, position.y, arrayOfOtherPoints[i].x, arrayOfOtherPoints[i].y) <= radius + arrayOfRadii[i] + 2 * CIRCLE_STROKE_WEIGHT) {
@@ -50,13 +101,15 @@ function touchingOtherCircle(position, radius, arrayOfOtherPoints, arrayOfRadii)
     return false;
 }
 
-function distanceOfPathFromIndexArray(indexArrayForDistance) {
+// given a certain order of indices as an array, it will calculate 
+// the distance of that path throughout the array of points
+function distanceGivenIndexOrder(order) {
     var runningDist = 0;
-    for(var i = 0; i < indexArrayForDistance.length; i++) {
-        if(indexArrayForDistance[i + 1] == null) {
+    for(var i = 0; i < order.length; i++) {
+        if(order[i + 1] == null) {
             return runningDist;
         }
-        runningDist += dist(points[indexArrayForDistance[i]].x, points[indexArrayForDistance[i]].y, points[indexArrayForDistance[i + 1]].x, points[indexArrayForDistance[i + 1]].y);
+        runningDist += points[order[i]].dist(points[order[i + 1]]);
     }
     return runningDist;
 }
@@ -66,14 +119,21 @@ function draw() {
     
     drawTopBar();
     
+    // if the player has selected at least 
+    // 2 points, display the distance
     if(playerIndexOrder[1] != null) {
         textSize(TOP_BAR_HEIGHT * 0.25)
         textAlign(LEFT, BOTTOM);
         noStroke();
         fill(255);
-        text("Your distance: " + nf(distanceOfPathFromIndexArray(playerIndexOrder), 0, 2), CIRCLE_STROKE_WEIGHT * 2, height - CIRCLE_STROKE_WEIGHT * 2);
+        text("Your distance: " +
+            nf(distanceGivenIndexOrder(playerIndexOrder), 0, 2),
+            CIRCLE_STROKE_WEIGHT * 2, 
+            height - CIRCLE_STROKE_WEIGHT * 2
+        );
     }
     
+    // if the best path should be drawn, draw it
     if(shouldDrawBestPath) {
         if(bestPath == -1) {
             bestPath = bestIndexPath();
@@ -81,8 +141,12 @@ function draw() {
             drawPathFromIndexOrder(bestPath, CIRCLE_STROKE_WEIGHT * 5, color(255, 255, 0));
         }
     }
+    
+    // draws the player's path
     drawPathFromIndexOrder(playerIndexOrder);
     
+    // draws the points based on if 
+    // they have been "selected" or not
     fill(20);
     stroke(color(50, 168, 82));
     strokeWeight(CIRCLE_STROKE_WEIGHT);
@@ -91,12 +155,16 @@ function draw() {
         circle(points[i].x, points[i].y, pointsRadii[i] * 2);
     }
     
+    // if the player has selected to check their path, 
+    // it will change shouldDrawBestPath variable to 
+    // true and display some information about their 
+    // path and the optimal path
     if(shouldDrawBestPath) {
         textSize(TOP_BAR_HEIGHT * 0.25)
         textAlign(RIGHT, BOTTOM);
         noStroke();
         fill(color(255, 255, 0));
-        text("Optimal distance: " + nf(distanceOfPathFromIndexArray(bestPath), 0, 2), width - CIRCLE_STROKE_WEIGHT * 2, height - CIRCLE_STROKE_WEIGHT * 2);
+        text("Optimal distance: " + nf(distanceGivenIndexOrder(bestPath), 0, 2), width - CIRCLE_STROKE_WEIGHT * 2, height - CIRCLE_STROKE_WEIGHT * 2);
         
         textAlign(CENTER, BOTTOM);
         
@@ -104,7 +172,7 @@ function draw() {
         if(pathsAreEquivalent(bestPath, playerIndexOrder)) {
             stringToDisplay = "You got the optimal path!"; 
         } else {
-            stringToDisplay = "Your path is " + nf((distanceOfPathFromIndexArray(playerIndexOrder) / distanceOfPathFromIndexArray(bestPath) - 1) * 100, 0, 2) + "% longer";
+            stringToDisplay = "Your path is " + nf((distanceGivenIndexOrder(playerIndexOrder) / distanceGivenIndexOrder(bestPath) - 1) * 100, 0, 2) + "% longer";
         }
         fill(255, 100);
         rect(width / 2 - ((textWidth(stringToDisplay) + CIRCLE_STROKE_WEIGHT * 3) / 2), height - CIRCLE_STROKE_WEIGHT * 2 - (TOP_BAR_HEIGHT * 0.25 * 1.1), textWidth(stringToDisplay) + CIRCLE_STROKE_WEIGHT * 3, TOP_BAR_HEIGHT * 0.25 * 1.1);
@@ -113,11 +181,13 @@ function draw() {
     }
 }
 
+// returns whether the paths are truly equivalent
 function pathsAreEquivalent(indexPath1, indexPath2) {
-    return pathsAreEquivalentSpecific(indexPath1, indexPath2) || pathsAreEquivalentSpecific(reverse(indexPath1), indexPath2)
+    return pathsAreEquivalentSpecificOrder(indexPath1, indexPath2) || pathsAreEquivalentSpecificOrder(reverse(indexPath1), indexPath2)
 }
 
-function pathsAreEquivalentSpecific(indexPath1, indexPath2) {
+// returns whether the paths are truly equivalent with order dependency
+function pathsAreEquivalentSpecificOrder(indexPath1, indexPath2) {
     if(indexPath1.length == indexPath2.length) {
         for(var i = 0; i < indexPath1.length; i++) {
             if(indexPath1[i] != indexPath2[i]) {
@@ -130,6 +200,7 @@ function pathsAreEquivalentSpecific(indexPath1, indexPath2) {
     }
 }
 
+// draws the top bar and it's buttons
 function drawTopBar() {
     noStroke();
     fill(color(50, 168, 162));
@@ -154,10 +225,10 @@ function drawTopBar() {
     fill(255);
     text("Clear", width * 5 / 6, TOP_BAR_HEIGHT * 0.45);
     
-    fill(255);
     text("Reset", width / 2, TOP_BAR_HEIGHT * 0.45);
 }
 
+// draws a path given the index order that the path should be drawn in
 function drawPathFromIndexOrder(indexOrderToDraw, pathStrokeWeight=CIRCLE_STROKE_WEIGHT*2, pathStrokeColor=color(50, 168, 162)) {
     beginShape();
     for(var i = 0; i < indexOrderToDraw.length; i++) {
@@ -175,6 +246,8 @@ function factorial(n) {
     return n == 1 ? 1 : n * factorial(n - 1);
 }
 
+// returns an array of indices of all the 
+// displayed circles that the mouse is over
 function arrayOfIndicesMouseIsOver() {
     var toReturn = [];
     for(var i = 0; i < NUMBER_OF_POINTS; i++) {
@@ -185,12 +258,14 @@ function arrayOfIndicesMouseIsOver() {
     return toReturn;
 }
 
+// clears the player's chosen path
 function clearPoints() {
     playerIndexOrder = new Array(NUMBER_OF_POINTS).fill(null);
     shouldDrawBestPath = false;
     currentIndexInPlayerOrder = 0;
 }
 
+// randomizes the points just like at the beginning of the program in setup
 function resetPoints() {
     points = Array(NUMBER_OF_POINTS);
     pointsRadii = Array(NUMBER_OF_POINTS);
@@ -206,13 +281,21 @@ function resetPoints() {
     const MINIMUM_RADIUS = 25 / 700 * height;
     const MAXIMUM_RADIUS = 50 / 700 * height;
     
+    // for comments, reference setup()
     for(var i = 0; i < NUMBER_OF_POINTS; i++) {
-        playerIndexOrder[i] = null;
-        proposedRadius = random(MINIMUM_RADIUS, MAXIMUM_RADIUS);
-        proposedPoint = createVector(random(proposedRadius + CIRCLE_STROKE_WEIGHT, width - proposedRadius - CIRCLE_STROKE_WEIGHT), random(proposedRadius + CIRCLE_STROKE_WEIGHT + TOP_BAR_HEIGHT, height - proposedRadius - CIRCLE_STROKE_WEIGHT));
+        var proposedRadius = random(MINIMUM_RADIUS, MAXIMUM_RADIUS);
+        const topLeft = createVector(
+            proposedRadius + CIRCLE_STROKE_WEIGHT, 
+            proposedRadius + CIRCLE_STROKE_WEIGHT + TOP_BAR_HEIGHT
+        ); 
+        const bottomRight = createVector(
+            width - proposedRadius - CIRCLE_STROKE_WEIGHT, 
+            height - proposedRadius - CIRCLE_STROKE_WEIGHT
+        )
+        var proposedPoint = randomPointInBounds(topLeft, bottomRight);
         var whileCounter = 0;
         while(touchingOtherCircle(proposedPoint, proposedRadius, points, pointsRadii) && whileCounter < 50) {
-            proposedPoint = createVector(random(proposedRadius + CIRCLE_STROKE_WEIGHT, width - proposedRadius - CIRCLE_STROKE_WEIGHT), random(proposedRadius + CIRCLE_STROKE_WEIGHT + TOP_BAR_HEIGHT, height - proposedRadius - CIRCLE_STROKE_WEIGHT));
+            proposedPoint = randomPointInBounds(topLeft, bottomRight);
             whileCounter++;
         }
         points[i] = proposedPoint;
@@ -220,8 +303,11 @@ function resetPoints() {
     }
 }
 
+// checks if any of the buttons are pressed or 
+// if any of the circles are being selected
 function mousePressed() {
     if(mouseY <= TOP_BAR_HEIGHT) {
+        /*|      CHECK        |      RESET       |       CLEAR       |*/
         if(mouseX < width / 3) {
             if(playerIndexOrder.indexOf(null) == -1) {
                 shouldDrawBestPath = true;
@@ -234,10 +320,14 @@ function mousePressed() {
     } else {
         if(arrayOfIndicesMouseIsOver().length != 0) {
             const trueIndex = max(arrayOfIndicesMouseIsOver());
+            // checks if the circle that was selected is already selected
             if(playerIndexOrder.indexOf(trueIndex) == -1) {
+                // if the circle is not selected, select it
                 playerIndexOrder[currentIndexInPlayerOrder] = trueIndex;
                 currentIndexInPlayerOrder++;
             } else {
+                // if the circle is selected, make that 
+                // the last selected point in the player's path
                 currentIndexInPlayerOrder = playerIndexOrder.indexOf(trueIndex);
                 currentIndexInPlayerOrder += currentIndexInPlayerOrder != 0;
                 for(var i = currentIndexInPlayerOrder; i < NUMBER_OF_POINTS; i++) {
@@ -249,17 +339,24 @@ function mousePressed() {
     }
 }
 
+// returns an array of indices that represents 
+// the most optimal path through the given points
 function bestIndexPath() {
     var bestPathSoFar = Array(NUMBER_OF_POINTS).fill().map((x,i)=>i);
-    var bestDistanceSoFar = distanceOfPathFromIndexArray(bestPathSoFar);
-    
+    var bestDistanceSoFar = distanceGivenIndexOrder(bestPathSoFar);
     var currentIndexOrder = bestPathSoFar.slice();
-    var currentDistance = distanceOfPathFromIndexArray(currentIndexOrder);
+    var currentDistance = distanceGivenIndexOrder(currentIndexOrder);
+    
+    // goes through every single possibility of path orientation
     for(var i = 0; i < NUMBER_OF_PERMUTATIONS; i++) {
+        // if there is no next order iteration, you 
+        // have checked all of the possible paths
         if(nextIndexOrderIteration(currentIndexOrder) != -1) {
+            // iterate the current index order that is being inspected
             currentIndexOrder = nextIndexOrderIteration(currentIndexOrder);
-            if(distanceOfPathFromIndexArray(currentIndexOrder) < bestDistanceSoFar) {
-                bestDistanceSoFar = distanceOfPathFromIndexArray(currentIndexOrder);
+            // checks if the current path is more optimal than the current optimal path
+            if(distanceGivenIndexOrder(currentIndexOrder) < bestDistanceSoFar) {
+                bestDistanceSoFar = distanceGivenIndexOrder(currentIndexOrder);
                 bestPathSoFar = currentIndexOrder.slice();
             }
         } else {
@@ -268,7 +365,7 @@ function bestIndexPath() {
     }
 }
 
-// With help from Michal Forišek's explaination
+// With help from Michal Forišek's explanation
 // https://www.quora.com/How-would-you-explain-an-algorithm-that-generates-permutations-using-lexicographic-ordering
 function nextIndexOrderIteration(indexOrder) {
     var toReturn = indexOrder.slice();
@@ -295,6 +392,7 @@ function nextIndexOrderIteration(indexOrder) {
     return toReturn;
 }
 
+// swaps two elements of an array
 function swap(arr, i1, i2) {
     var temp = arr[i1]
     arr[i1] = arr[i2];
